f8865f9ce71a9ca2f8d7a69e51420656b35f3275
MernaNashaat <mernanashaat27@gmail.com>
2020-06-18 23:01:21 +0200

    styling om el course cards section 3shan zh22222t

--- src/components/cards/courseCard.jsx
+++ src/components/cards/courseCard.jsx
@@ -18,14 +18,15 @@ const CourseCard = (props) => {
                 <div className="CourseCard--box">
 
                     <p className="CourseCard__text CourseCard__text--font ">Learning How To Learn: Powerful mental tools to help you master ... </p>
-                    <div className="CourseCard__items-container">
+                   
+                </div>
+                <div className="CourseCard__items-container">
                         <span className="CourseCard__text CourseCard__text--space CourseCard__text--font">Expert</span>
                         <div className="CourseCard__icon-container">
                             <AiOutlineTeam className="CourseCard__icon" />
                             <span className="CourseCard__text CourseCard__text--space CourseCard__text--font">100</span>
                         </div>
                     </div>
-                </div>
                 <div className="CourseCard__items-container">
                     <span className="CourseCard__text CourseCard__text--space CourseCard__text--font">Duration : 2H </span>
                     <span className="CourseCard__text CourseCard__text--space CourseCard__text--font"> $149.99 </span>
@@ -49,23 +50,7 @@ const CourseCard = (props) => {
                      </div>
                     }
                     </div>
-                    <div className="CourseCard__items-container">
-                        <div className="CourseCard__stars-container">
-                            <div className="CourseCard__text CourseCard__text--space">
-
-                                {stars.map(star => <AiFillStar className="CourseCard__star" ></AiFillStar>)}
-                                <AiOutlineStar className="CourseCard__star "></AiOutlineStar>
-                                {props.path === "/home" || props.path === "/freeCourses/:id" || props.path === "/paidCourses/:id" || props.path === "/enrolledCourses/:id" ?
-                                    <Link className=" CourseCard__btn CourseCard__text--font" to="/shoppingcart">Add to cart</Link>
-                                    :
-                                    <div className="crud">
-                                        <AiFillEdit className="crud__edit" />
-                                        <AiFillDelete className="crud__delete" />
-                                    </div>
-                                }
-                            </div>
-                        </div>
-                    </div>
+                   
                 </div>
                 </div>
             </div>
