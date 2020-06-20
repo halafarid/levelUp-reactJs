b6250bd0b712ba50adb3a4d68c2562f4db8103f0
halafarid <hala.farid179@gmail.com>
2020-06-21 01:34:51 +0200

    Features Service

---
+++ src/services/featuresService.js
@@ -0,0 +1,26 @@
+import { apiUrl } from './config.json';
+import axios from 'axios';
+
+// Beginner, Intermediate, Advanced
+export function getAllLevels() {
+    return axios.get(`${apiUrl}/levels`);
+};
+
+export function getAllCategories() {
+    return axios.get(`${apiUrl}/categories`);
+};
+
+// Search
+export function searchByInstructors() {
+    return axios.get(`${apiUrl}/search/instructors`);
+};
+
+export function searchByCourses() {
+    return axios.get(`${apiUrl}/search/courses`);
+};
+
+export function searchByJobs() {
+    return axios.get(`${apiUrl}/search/jobs`);
+};
+
+// Filters
