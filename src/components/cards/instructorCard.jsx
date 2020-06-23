fe1765e9a9d41908b0c1addc1527e4162686b18f
halafarid <hala.farid179@gmail.com>
2020-06-23 03:40:35 +0200

    Handle wishlist & instructor card

--- src/components/cards/instructorCard.jsx
+++ src/components/cards/instructorCard.jsx
@@ -9,29 +9,26 @@ const InstructorCard = props => {
   
     return ( 
         <React.Fragment>
-        <Container className="InstructorContainer">
-        <Link  className="InstructorContainer__link" to={`/profile/${id}`}>
-             <Card >
-                <Card.Img  className="card__card-img" src={require("../../images/instructor/photo.jpg")} alt="Instructor"/>
-                <Card.Body>
-                    <Card.Title className="card__card-title">
-                    {props.Instructor.name}</Card.Title>
-                    <Card.Text className="card__card-text">
-                      {props.Instructor.job}
-                     </Card.Text>
-                    <div className="rating">
-                    <AiFillStar/>
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiOutlineStar/>
-                    <span className="rating__num">{props.Instructor.rating}</span>
-                    </div>
-                    {/* <p className="card__card-text card__card-text--color"> {props.Instructor.reviews} Reviews </p> */}
-                </Card.Body>
-
-            </Card>
-        </Link>
+            <Container className="InstructorContainer">
+                <Link  className="InstructorContainer__link" to={`/profile/${id}`}>
+                    <Card >
+                        <Card.Img  className="card__card-img" src={require("../../images/instructor/photo.jpg")} alt="Instructor"/>
+                        <Card.Body>
+                            <Card.Title className="card__card-title">
+                            {props.Instructor.fullName}</Card.Title>
+                            <Card.Text className="card__card-text">
+                            {props.Instructor.job.title}
+                            </Card.Text>
+                            <div className="rating">
+                                <AiFillStar/>
+                                <AiFillStar />
+                                <AiFillStar />
+                                <AiFillStar />
+                                <AiOutlineStar/>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Link>
             </Container>
         </React.Fragment>
     );
